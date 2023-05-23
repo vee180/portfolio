2b366d433ab8e4594babcfe13f33d01e6468d980
vee180 <venerasultann@gmail.com>
2023-05-22 21:46:01 -0400

    root files changed

---
+++ template/mailer copy 4/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
+	$to = 'venerasultann@gmail.com'; // Enter Your E-mail
+	$from = 'admin@venera-sultan-portfolio.com'; // Admin e-mail
+	$fromName = 'Venera Sultan'; // Your company name
+	$charset = 'UTF-8';
+?>
